af196f65fc44d3d4ca52a21383b4991a295ff7dc
shravani shinde <sshravani255@gmail.com>
2023-10-16 17:30:56 +0530

    committed 10/16/2023, 5:30:56 PM

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,35 @@
+#include<stdio.h>
 
+int input() {
+    int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    return num;
+}
+
+int findLargest(int num1, int num2, int num3) {
+    int largest = num1;
+    if (num2 > largest) {
+        largest = num2;
+    }
+    if (num3 > largest) {
+        largest = num3;
+    }
+    return largest;
+}
+
+void displayResult(int largest) {
+    printf("The largest number is:%d\n",largest);
+}
+
+int main() {
+    int num1, num2, num3, largest;
+    num1 = input();
+    num2 = input();
+    num3 = input();
+    largest = findLargest(num1,num2,num3);
+    displayResult(largest);
+
+
+    return 0;
+}
